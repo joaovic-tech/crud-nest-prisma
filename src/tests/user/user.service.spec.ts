d91320fc5d43efe9d5b72b7343b829f4698f608a
joaovic <joaovictorca2004@gmail.com>
2025-12-30 18:18:38 -0300

    refactor(tests): v2

--- src/tests/user/user.service.spec.ts
+++ src/tests/user/user.service.spec.ts
@@ -4,26 +4,27 @@ import { PrismaService } from 'prisma.service';
 import { Logger } from '@nestjs/common';
 import { UserEntity } from 'modules/user/entities/user.entity';
 import { CreateUserDto } from 'modules/user/dto/create-user.dto';
+import { IUser } from 'modules/user/interface/user.interface';
 
 describe('UserService', () => {
-  const mockUser: CreateUserDto = {
+  const mockUserDTO: CreateUserDto = {
     email: 'teste@teste.com',
     name: 'tester',
     password: 'tester@s12',
   };
-  const mockUserToDB = {
+  const mockUserToDB: IUser = {
     id: 1,
-    ...mockUser,
+    ...mockUserDTO,
   };
   let service: UserService;
   const prismaMock = {
     user: {
-      create: jest.fn(),
+      create: jest.fn().mockResolvedValue(mockUserToDB),
       findMany: jest.fn(),
-      findUniqueOrThrow: jest.fn(),
-      findOne: jest.fn(),
-      update: jest.fn(),
-      delete: jest.fn(),
+      findUniqueOrThrow: jest.fn().mockResolvedValue(mockUserToDB),
+      findOne: jest.fn().mockResolvedValue(mockUserToDB),
+      update: jest.fn().mockResolvedValue(mockUserToDB),
+      delete: jest.fn().mockResolvedValue(mockUserToDB),
     },
   };
 
@@ -43,8 +44,6 @@ describe('UserService', () => {
   });
 
   it('Should create a user successfully with correct data', async () => {
-    prismaMock.user.create.mockResolvedValue(mockUserToDB);
-
     const result = await service.create(mockUserToDB);
     expect(result).toEqual(
       expect.objectContaining({
@@ -55,10 +54,7 @@ describe('UserService', () => {
   });
 
   it('Should hash the password before saving to database', async () => {
-    prismaMock.user.create.mockResolvedValue(mockUserToDB);
-
     await service.create(mockUserToDB);
-
     expect(prismaMock.user.create).toHaveBeenCalledWith(
       expect.objectContaining({
         data: expect.objectContaining({
@@ -81,8 +77,6 @@ describe('UserService', () => {
   });
 
   it('Should find a user by unique input', async () => {
-    prismaMock.user.findUniqueOrThrow.mockResolvedValue(mockUserToDB);
-
     const result = await service.findOne({ id: mockUserToDB.id });
 
     expect(result).toEqual({
@@ -96,25 +90,20 @@ describe('UserService', () => {
   });
 
   it('Should update user data', async () => {
-    prismaMock.user.update.mockResolvedValue(mockUserToDB);
-
-    const result = await service.update(mockUserToDB.id, mockUser);
+    const result = await service.update(mockUserToDB.id, mockUserDTO);
 
     expect(result.email).toEqual(mockUserToDB.email);
     expect(prismaMock.user.update).toHaveBeenCalledWith({
       where: { id: mockUserToDB.id },
-      data: mockUser,
+      data: mockUserDTO,
     });
   });
 
   it('Should delete user', async () => {
-    prismaMock.user.delete.mockResolvedValue(mockUserToDB);
-
     const createdUser = await service.create(mockUserToDB);
+    const result = await service.remove(mockUserToDB.id);
 
     expect(createdUser).not.toBeNull();
-
-    const result = await service.remove(mockUserToDB.id);
     expect(result).toEqual({
       message: `Usuário {${mockUserToDB.name}} deletado com sucesso!`,
     });
